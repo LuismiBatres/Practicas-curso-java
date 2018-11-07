d24c2a3e878b680208808cb3b2f2b46646d67360
LuismiBatres <luismi.sanchez1@gmail.com>
2018-11-07 11:56:59 +0100

    Creacion de practica 3

--- Practica3_Generics/src/Limite.java
+++ Practica3_Generics/src/Limite.java
@@ -12,7 +12,12 @@ public class Limite<T> {
 	}
 	
 	public static void main(String[] args) {
-		Limite<Integer>caja1=new Limite<Integer>();
-		Limite<Integer>caja2=new Limite<>();
+		Limite<Integer>caja1=new Limite<>();
+		caja1.set(34);
+		System.out.println(caja1.get());
+		
+		Limite<String>caja2=new Limite<>();
+		caja2.set("Hola");
+		System.out.println(caja2.get());
 	}
 }
