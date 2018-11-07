6ce3b2723be039cb2a4bf806c1242b86bf8e998b
LuismiBatres <luismi.sanchez1@gmail.com>
2018-11-07 13:08:00 +0100

    Ampliacion Practica 3ç

--- Practica3_Generics/src/Limite.java
+++ Practica3_Generics/src/Limite.java
@@ -10,13 +10,24 @@ public class Limite<T> {
 		this.t=t;
 	}
 	
+	public <U extends Number> void revision(U u){
+		 System.out.println("T: " + t.getClass().getName());
+	}
+		
+	
 	public static void main(String[] args) {
 		Limite<Integer>caja1=new Limite<>();
 		caja1.set(34);
 		System.out.println(caja1.get());
+		caja1.revision(34);
 		
-		Limite<String>caja2=new Limite<>();
-		caja2.set("Hola");
+		Limite<Double>caja2=new Limite<>();
+		caja2.set(25d);
 		System.out.println(caja2.get());
+		caja2.revision(28d);
+		
+		/*Limite<String> obj = new Limite<>();
+		obj.set("CadenaEjemplo");
+		obj.revision("Cadena ejemplo"); // error!!*/
 	}
 }
