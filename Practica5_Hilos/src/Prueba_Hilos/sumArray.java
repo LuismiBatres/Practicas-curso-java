23c16c9796f673420d538acbede88a3285a345d8
LuismiBatres <luismi.sanchez1@gmail.com>
2018-11-08 13:32:36 +0100

    Practica 5 Hilos

--- Practica5_Hilos/src/Prueba_Hilos/sumArray.java
+++ Practica5_Hilos/src/Prueba_Hilos/sumArray.java
@@ -8,7 +8,7 @@ public class sumArray {
 		sum = 0;
 		for (int i = 0; i < nums.length; i++) {
 			sum += nums[i];
-			System.out.println("Total acumulado de " + Thread.currentThread().getName() + " es" + sum);
+			System.out.println("Total acumulado de " + Thread.currentThread().getName() + " es " + sum);
 			try {
 				Thread.sleep(10);// permitir el cambio de tarea
 			} catch (InterruptedException exc) {
